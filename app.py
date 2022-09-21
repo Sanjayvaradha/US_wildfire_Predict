3b1048bdd52004b940fd56964897b876f00156fe
Sanjay varadharajan <70106311+Sanjayvaradha@users.noreply.github.com>
2022-09-21 15:45:20 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+import zipfile
+
+folder_to_be_unzipped = 'models_unzipped'
+with zipfile.ZipFile('rf_models.zip', 'r') as zip_ref:
+    zip_ref.extractall(folder_to_be_unzipped)
+
+
+# Create an app object using the Flask class.
+app = Flask(__name__)
+
+# Load the trained model. (Pickle file)
+model = pickle.load(
+    open("models_unzipped\\rf_model\\randomforest_model.pkl", 'rb'))
+
+causes = {}
+incidents = ['Natural', 'Accidental', 'Malicious', 'Other']
+
+for i, cause in enumerate(incidents):
+    causes[i] = cause
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    probability = []
+    # Convert string inputs to float.
+    int_features = [float(x) for x in request.form.values()]
+    features = np.array(int_features)
+    features = features.reshape(1, 6)
+
+    temp = model.predict_proba(features)
+
+    for i in range(len(temp)):
+        probability.append(temp[i][0][1])
+
+    maximum = max(probability)
+    index_num = probability.index(maximum)
+    output = causes[index_num]
+    print(output)
+
+    return render_template('index.html', prediction_text='Fire caused by {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run()
